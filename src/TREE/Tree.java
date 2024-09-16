c4d5c6a5490de92457be83c96f2b18965966c651
syedmdfarhaneazam <syedmdfarhaneazam@gmail.com>
2024-09-17 01:31:48 +0530

    Size,Height,Max,Min
    added basic functions in Tree.java like size , height , max value of a node , min value of a node

--- src/TREE/Tree.java
+++ src/TREE/Tree.java
@@ -16,4 +16,20 @@ public class Tree{
         root.right = createTree();
         return root;
     }
+    public static int height(treeNode root){ //to calculate the max height
+        if(root == null ) return 0; //base case
+        return Math.max(height(root.left) , height(root.right)) +1; //to sent the funtion on either side and then add one for the fact that the current node is not null
+    }
+    public static int size(treeNode root){ //to calculate the total number of nodes in the  tree
+        if(root == null ) return 0; //base case since we are not gonna count null nodes
+        return size(root.right)+size(root.left)+1; //adding one for the current node and then sending the funtion to the either side of the node
+    }
+    public static int maxNode(treeNode root){
+        if(root == null) return Integer.MIN_VALUE; //base case return the min value so that it gets discarded
+        return Math.max(root.value , (Math.max(maxNode(root.right), maxNode(root.left)))); //return the min
+    }
+    public static int minNode(treeNode root){
+        if (root == null) return Integer.MAX_VALUE; // base case returns the max value and gets discarded 
+        return Math.min(root.value , (Math.min(minNode(root.right), minNode(root.left)))); //returns the min value between left right and the current node
+    }
 }
--- src/TREE/treeUse.java
+++ src/TREE/treeUse.java
@@ -5,11 +5,16 @@ public class treeUse{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in); //initializing the scanner object
         treeNode root = Tree.createTree();
-        System.out.println("INORDER");
-        traversal.inOrder(root);
-        System.out.println("PREORDER");
-        traversal.preOrder(root);
-        System.out.println("POSTORDER");
-        traversal.postOrder(root);
+        /****** FOR TRAVERSAL *****/
+        //System.out.println("INORDER");
+        //traversal.inOrder(root);
+        //System.out.println("PREORDER");
+        //traversal.preOrder(root);
+        //System.out.println("POSTORDER");
+        //traversal.postOrder(root);
+        /******HEIGHT****** */
+        System.out.println(Tree.height(root));
+        /******SIZE***** */
+        System.out.println(Tree.size(root));
     }
 }
